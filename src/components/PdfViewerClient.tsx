130e7241020b13dbbed91ca26185ceee4b4452de
dhiksn <andhikarafi321@gmail.com>
2026-09-18 18:48:32 +0700

    modified:   src/app/pdf/[slug]/page.tdwadawdawdwada

--- src/components/PdfViewerClient.tsx
+++ src/components/PdfViewerClient.tsx
@@ -64,59 +64,6 @@ export default function PdfViewerClient({
           />
         </div>
       </div>
-
-      {/* Custom Controls */}
-      <div className="absolute bottom-5 left-1/2 z-50 flex -translate-x-1/2 items-center gap-1 rounded-2xl border border-white/10 bg-black/70 p-1.5 shadow-2xl backdrop-blur-xl">
-        {/* Zoom Out */}
-        <button
-          onClick={zoomOut}
-          disabled={zoom <= 50}
-          className="flex h-10 w-10 items-center justify-center rounded-xl text-white transition hover:bg-white/10 disabled:cursor-not-allowed disabled:opacity-30"
-          title="Zoom Out"
-        >
-          <ZoomOut size={19} />
-        </button>
-
-        {/* Zoom Percentage */}
-        <button
-          onClick={resetZoom}
-          className="min-w-[55px] rounded-xl px-2 py-2 text-sm font-medium text-white transition hover:bg-white/10"
-          title="Reset Zoom"
-        >
-          {zoom}%
-        </button>
-
-        {/* Zoom In */}
-        <button
-          onClick={zoomIn}
-          disabled={zoom >= 200}
-          className="flex h-10 w-10 items-center justify-center rounded-xl text-white transition hover:bg-white/10 disabled:cursor-not-allowed disabled:opacity-30"
-          title="Zoom In"
-        >
-          <ZoomIn size={19} />
-        </button>
-
-        {/* Divider */}
-        <div className="mx-1 h-6 w-px bg-white/10" />
-
-        {/* Reset */}
-        <button
-          onClick={resetZoom}
-          className="flex h-10 w-10 items-center justify-center rounded-xl text-white transition hover:bg-white/10"
-          title="Reset Zoom"
-        >
-          <RotateCcw size={18} />
-        </button>
-
-        {/* Fullscreen */}
-        <button
-          onClick={fullscreen}
-          className="flex h-10 w-10 items-center justify-center rounded-xl text-white transition hover:bg-white/10"
-          title="Fullscreen"
-        >
-          <Maximize size={18} />
-        </button>
-      </div>
     </div>
   );
 }
